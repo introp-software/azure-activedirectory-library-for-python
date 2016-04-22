fe2c28135503af22f513d6b1c1b20b2fd8beb0ee
Prasanna <prasanna@introp.net>
2016-04-23 00:07:13 +1000

    Implemented the code for authrequest

--- adalpython/Client.py
+++ adalpython/Client.py
@@ -1,4 +1,8 @@
 from .httpclient import httpclient
+import jwt
+import urllib
+import webbrowser
+from urllib.parse import unquote, urlparse, urlencode, ParseResult
 """description of class"""
     
 """ Need to add licensing terms here 
@@ -30,8 +34,14 @@ class Client(object):
       promptlogin(bool,optional) : Whether to prompt for login or use existing session.
      
      '''
-    def authrequest(stateparams=[], extraparams=[],promptlogin = False):
-     return ""
+    def authrequest(self,promptlogin = False,stateparams=[], extraparams=[]):
+        try:
+         url = "https://login.microsoftonline.com/common/oauth2/authorize"
+         params = self.get_authrequest_parameters(stateparams,extraparams)
+         url = self.add_url_params(url,params)
+        except Exception as ex:
+         return ex.args[0]
+        webbrowser.open(url)
 
     ''' Handle auth response.
       Args:
@@ -51,8 +61,9 @@ class Client(object):
      * @param string $expectednonce Expected nonce to validate received nonce against.
      * @return \microsoft\adalphp\OIDC\IDTokenInterface An IDToken object.
      */'''
-    def process_idtoken(idtoken, expectednonce = ''):
-     return ""
+    def process_idtoken(self,idtoken, expectednonce = ''):
+        details = jwt.decode(idtoken,False,None,None)
+        return details
 
     '''
   /**
@@ -142,6 +153,64 @@ class Client(object):
     
     def get_authflow(self):
        return self._authflow
+
+    def get_authrequest_parameters(self, stateparams, extraparams):
+        params = {'scope':'openid'}
+        params['client_id'] = self.get_clientid()
+        params['redirect_uri'] = self.get_redirecturi()
+        params['response_type'] = 'code'
+        params['response_mode'] = 'form_post'
+        params['resource'] = self.get_resource()
+        if stateparams is not None :
+            stateparam = ''
+            for state in stateparams:
+              stateparam = stateparam +"," + state
+            params['state'] = stateparam
+        if extraparams is not None:
+            for extra in extraparams:
+                params[extra] = extra
+        return params
+
+    def add_url_params(self,url, params):
+     """ Add GET params to provided URL being aware of existing.
+
+     :param url: string of target URL
+     :param params: dict containing requested params to be added
+     :return: string with updated URL
+
+     >> url = 'http://stackoverflow.com/test?answers=true'
+     >> new_params = {'answers': False, 'data': ['some','values']}
+     >> add_url_params(url, new_params)
+     'http://stackoverflow.com/test?data=some&data=values&answers=false'
+     """
+     # Unquoting URL first so we don't loose existing args
+     url = unquote(url)
+     # Extracting url info
+     parsed_url = urlparse(url)
+     # Extracting URL arguments from parsed URL
+     get_args = parsed_url.query
+     # Converting URL arguments to dict
+     parsed_get_args = dict(parse_qsl(get_args))
+     # Merging URL arguments dict with new params
+     parsed_get_args.update(params)
+ 
+     # Bool and Dict values should be converted to json-friendly values
+     # you may throw this part away if you don't like it :)
+     parsed_get_args.update(
+        {k: dumps(v) for k, v in parsed_get_args.items()
+         if isinstance(v, (bool, dict))}
+     )
+
+     # Converting URL argument to proper query string
+     encoded_get_args = urlencode(parsed_get_args, doseq=True)
+     # Creating new parsed result object based on provided with new
+     # URL arguments. Same thing happens inside of urlparse.
+     new_url = ParseResult(
+        parsed_url.scheme, parsed_url.netloc, parsed_url.path,
+        parsed_url.params, encoded_get_args, parsed_url.fragment
+     ).geturl()
+
+     return new_url
 
 
 class _DefaultValues:
