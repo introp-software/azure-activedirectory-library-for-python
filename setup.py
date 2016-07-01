e735354dca28d56c745e6bee5504a24ff4d54ec6
Prasanna <prasanna@introp.net>
2016-07-01 17:13:08 +1000

    Added setup.py for library.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='adalpython',
+    version='1',
+    packages=['adalpython', 'adalpython.tests'],
+    url='',
+    license='',
+    author='Prasanna',
+    author_email='prasanna@introp.net',
+    description=''
+)
